4b1dafbf6542936e11392c4265769b241bc44b21
ahmed magdy <66494182+EngAhmedMagdy@users.noreply.github.com>
2022-12-28 07:27:30 +0200

    Update median_filter_colored.m

--- median_filter_colored.m
+++ median_filter_colored.m
@@ -3,15 +3,14 @@ clear
 i = imread("me.jpg");
 N = imnoise(i,'salt & pepper',0.5);
 
+R = N(:,:,1)
+G = N(:,:,2)
+B = N(:,:,3)
 
-red_channel = medfilt2(N(:,:,1),[3 3]);
-green_channel = medfilt2(N(:,:,2),[3 3]);
-blue_channel = medfilt2(N(:,:,3),[3 3]);
 
-
-red_channel = medfilt2(i(:,:,1),[3 3]);
-green_channel = medfilt2(i(:,:,2),[3 3]);
-blue_channel = medfilt2(i(:,:,3),[3 3]);
+red_channel = medfilt2(R,[3 3]);
+green_channel = medfilt2(G,[3 3]);
+blue_channel = medfilt2(B,[3 3]);
 
 noise_free = cat(3,red_channel,green_channel,blue_channel);
 
